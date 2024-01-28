f4ed1d57d1eb7d238570550cf2f59020ab3a7705
Franck polla <fpolla640@gmail.com>
2024-01-28 17:39:03 +0200

    i created the footer component

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,8 +1,61 @@
 import React from "react";
 import "./footer.css";
-
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
+import { BsWhatsapp } from "react-icons/bs";
 function footer() {
-  return <div>footer</div>;
+  return (
+    <footer>
+      <a href="" className="footer_logo">
+        {" "}
+        Franck Polla
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href="https://wa.me/+905338365434 " target="_blank">
+          <BsWhatsapp />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/franck-polla-ba9976288/"
+          target="_blank"
+        >
+          <FaLinkedinIn />
+        </a>
+        <a href="https://github.com/franckpolla" target="_blank">
+          <FaGithub />
+        </a>
+      </div>
+      <div className="footer_copyright">
+        <small>
+          {" "}
+          © {new Date().getFullYear()} Franck Polla. All rights reserved.
+        </small>
+      </div>
+    </footer>
+  );
 }
 
 export default footer;
